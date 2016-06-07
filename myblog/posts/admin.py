2746e72ac3dfc290f0a4f63ab32744327e03c7c9
hlubega <ham_lubega@ymail.com>
2016-06-07 23:29:29 +0300

    First commit

---
+++ myblog/posts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+class PostModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Post
+admin.site.register(Post, PostModelAdmin)
